b6e6410c9adf7e2d1ea01bae80c6dfe4ea498299
moeidahmed <64657224+moeidahmed@users.noreply.github.com>
2022-11-17 12:36:57 +0500

    Update script.js

--- views/script.js
+++ views/script.js
@@ -1,16 +1,1 @@
-function getData() {
-  fetch("http://localhost:3000/index/data").then((res) => {
-    console.log(res);
-  });
 
-  //   let xhttp = new XMLHttpRequest();
-  //   let url = "http://localhost:3000/index/data";
-  //   xhttp.onreadystatechange = function () {
-  //     if (this.readyState == 4 && this.status == 200) {
-  //       let data = JSON.parse(this.responseText);
-  //       console.log(data);
-  //     }
-  //   };
-  //   xhttp.open("GET", url, true);
-  //   xhttp.send();
-}
